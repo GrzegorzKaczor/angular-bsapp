d6e48743f9766db4fe3366bfbf05d72066ce8526
Grzesiek Kaczor <grzechu.kaczor@gmail.com>
2020-07-26 06:31:31 +0200

    Add header and content components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CoreModule} from "./core/core.module";
+import {RouterModule, Routes} from "@angular/router";
+import { AllContractsComponent } from './all-contracts/all-contracts.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ActiveContractsComponent } from './active-contracts/active-contracts.component';
+import { SystemsComponent } from './systems/systems.component';
+import { AddContractComponent } from './add-contract/add-contract.component';
+import { AboutComponent } from './about/about.component';
+
+export const appRouters: Routes = [
+  {path: '', component: WelcomeComponent},
+  {path: 'allContracts', component: AllContractsComponent},
+  {path: 'activeContracts', component: ActiveContractsComponent},
+  {path: 'systems', component: SystemsComponent},
+  {path: 'addContract', component: AddContractComponent},
+  {path: 'about', component: AboutComponent},
+  {path: "**", component: WelcomeComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllContractsComponent,
+    WelcomeComponent,
+    ActiveContractsComponent,
+    SystemsComponent,
+    AddContractComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    RouterModule.forRoot(appRouters)
   ],
   providers: [],
   bootstrap: [AppComponent]
